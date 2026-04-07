4f115b572d543518bf1d976d1becaea3068bfce0
Rishabh Ballal <rishabhballal@gmail.com>
2026-04-07 09:50:55 +0530

    feat: add data visuals; refactor: split correlation function

--- analysis.py
+++ analysis.py
@@ -1,5 +1,7 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 def preprocess(X_train, X_test, imputer, scaler):
@@ -14,51 +16,120 @@ def preprocess(X_train, X_test, imputer, scaler):
     return X_train_imp, X_test_imp, X_train_scal, X_test_scal
 
 
-def select_feat_by_median(X_train, y_train, n=10):
-    median_s = X_train[y_train == 's'].median()
-    median_b = X_train[y_train == 'b'].median()
-    features = (median_s - median_b).abs().nlargest(n)
-    if not features.empty:
-        print(f'Classification differences in median (top {n})\n')
-        print(features.reset_index().to_string(header=False))
-    return features
+def _series_barplot(series, title, diverging=False, left_adjust=0):
+    palette = 'RdBu' if diverging else 'crest'
 
-
-def select_feat_by_correlation(X_train, y_train, n=10, threshold=0.75):
-    # feature-target linear correlations
-    y_train_binary = y_train.map(lambda x: 1 if x == 's' else 0)
-    feat_target_corr = X_train.agg(y_train_binary.corr).abs().nlargest(n)
-    print(f'\nFeature-target correlation (top {n})')
-    print(feat_target_corr.reset_index().to_string(header=False))
-
-    # feature-feature linear correlations
-    feat_corr_matrix = X_train[feat_target_corr.index].corr()
-    feat_corr_matrix = pd.DataFrame(
-        np.triu(feat_corr_matrix.values),
-        index=feat_corr_matrix.index,
-        columns=feat_corr_matrix.columns
+    plt.figure(figsize=(8, 6))
+    ax = sns.barplot(
+        x=series.values,
+        y=series.index,
+        hue=series.values,
+        palette=palette,
+        legend=False
     )
-    high_corr_feat = feat_corr_matrix.stack().reset_index()
-    high_corr_feat.columns = ['feature_1', 'feature_2', 'correlation']
-    high_corr_feat = high_corr_feat[
-        (high_corr_feat['correlation'].abs() > threshold)
-        & (high_corr_feat['feature_1'] != high_corr_feat['feature_2'])
-    ].reset_index(drop=True)
-    if not high_corr_feat.empty:
-        print(f'\nFeature-feature correlation (threshold = {threshold})')
-        print(high_corr_feat.to_string(header=False))
 
-    # unselect strongly correlated features
+    ax.set(title=title, xlabel=None, ylabel=None)
+    if left_adjust:
+        plt.subplots_adjust(left=left_adjust)
+
+    plt.savefig(f'vis/{title.replace(' ', '_')}.png')
+    plt.show()
+
+
+def mean_differences(X_train, y_train, plot_top_n=0, left_adjust=0):
+    mean_0 = X_train[y_train == 0].mean()
+    mean_1 = X_train[y_train == 1].mean()
+    feat_mean_diffs = (mean_1 - mean_0).abs().sort_values(ascending=False)
+
+    if plot_top_n:
+        _series_barplot(
+            series=feat_mean_diffs[:plot_top_n],
+            title=f'Top {plot_top_n} mean differences between classes',
+            left_adjust=left_adjust
+        )
+
+    return feat_mean_diffs
+
+
+def median_differences(X_train, y_train, plot_top_n=0, left_adjust=0):
+    median_0 = X_train[y_train == 0].median()
+    median_1 = X_train[y_train == 1].median()
+    feat_median_diffs = (median_1 - median_0).abs().sort_values(ascending=False)
+
+    if plot_top_n:
+        _series_barplot(
+            series=feat_median_diffs[:plot_top_n],
+            title=f'Top {plot_top_n} median differences between classes',
+            left_adjust=left_adjust
+        )
+
+    return feat_median_diffs
+
+
+def feature_target_correlation(X_train, y_train, plot_top_n=0, left_adjust=0):
+    feat_target_corr = X_train.agg(y_train.corr).sort_values(
+        ascending=False,
+        key=lambda x: x.abs()
+    )
+
+    if plot_top_n:
+        _series_barplot(
+            series=feat_target_corr[:plot_top_n],
+            title=f'Top {plot_top_n} feature-target correlations',
+            diverging=True,
+            left_adjust=left_adjust
+        )
+
+    return feat_target_corr
+
+
+def feature_feature_correlation(X_train, plot_top_n=0, adjust=0):
+    corr_matrix = X_train.corr()
+
+    feat_feat_corr = corr_matrix.stack().reset_index()
+    feat_feat_corr.columns = ['feature_1', 'feature_2', 'correlation']
+    feat_feat_corr = feat_feat_corr[
+        feat_feat_corr['feature_1'] != feat_feat_corr['feature_2']
+    ].sort_values(
+        'correlation',
+        ascending=False,
+        key=lambda x: x.abs()
+    )
+    top_n_corr = feat_feat_corr[:2*plot_top_n] # due to matrix symmetry
+    high_corr_feat = top_n_corr['feature_1'].drop_duplicates(ignore_index=True)
+    high_corr_matrix = corr_matrix.loc[high_corr_feat, high_corr_feat]
+
+    if plot_top_n:
+        plt.figure(figsize=(9, 7))
+        ax = sns.heatmap(
+            high_corr_matrix,
+            vmin=-1,
+            vmax=1,
+            cmap='RdBu',
+            center=0
+        )
+
+        ax.set(
+            title=f'Top {plot_top_n} feature-feature correlations',
+            xlabel=None,
+            ylabel=None)
+        if adjust:
+            plt.subplots_adjust(left=adjust, bottom=adjust)
+
+        plt.savefig(f'vis/Top_{plot_top_n}_feature-feature_correlations.png')
+        plt.show()
+
+    return feat_feat_corr
+
+
+def drop_highly_intercorrelated(feat_prop, feat_feat_corr, threshold=0.9):
+    high_corr_feat = feat_feat_corr[feat_feat_corr['correlation'] >= threshold]
+
     drop_feat = high_corr_feat.apply(
-        lambda row: row['feature_1'] if feat_target_corr[row['feature_1']]
-        < feat_target_corr[row['feature_2']] else row['feature_2'],
+        lambda row: row['feature_1']
+        if feat_prop[row['feature_1']] < feat_prop[row['feature_2']]
+        else row['feature_2'],
         axis=1
     ).drop_duplicates()
-    if not drop_feat.empty:
-        features = feat_target_corr.drop(index=drop_feat)
-        print('\nFeatures dropped from feature-target correlation table')
-        print(drop_feat.reset_index(drop=True))
-    else:
-        features = feat_target_corr
 
-    return features
+    return feat_prop.drop(index=drop_feat)
